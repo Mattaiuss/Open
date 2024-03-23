02f6717d020b6f33515d52b96c606bad85b53483
Sarah Nizar <sarah.nizar@epitech.eu>
2024-03-23 18:56:33 +0100

    [ADD] fin 6

--- src/endings/end_goku.c
+++ src/endings/end_goku.c
@@ -15,6 +15,7 @@ int end_goku(sfRenderWindow* window, sfEvent event)
     //     metre la chanson de "cha-la-head-cha-la.ogg"
     //     apres 5 second arrete puis metre le song "HEY_ITS_ME_GOKU.ogg"
     //     if(on ouvre la porte avec les etapes précédentes)
-    //     goku fonce sur nous en courrant avec le song "goku_drip.ogg"
+    //     rien derriere la porte, pendant 1sec
+    //     puis, goku fonce sur nous en courrant avec le song "goku_drip.ogg"
     return 0;
 }
