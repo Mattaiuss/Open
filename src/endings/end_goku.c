4a6da00ed2e29aa84459912c625b44e3230dec0f
Galvaing Valentin <valentin.galvaing@epitech.eu>
2024-03-23 18:54:52 +0100

    focntion goku beta

---
+++ src/endings/end_goku.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** Open
+** File description:
+** end_1
+*/
+
+#include "open.h"
+
+
+int end_goku(sfRenderWindow* window, sfEvent event)
+{
+    // if (la télé est cliqué);
+    //     metre la photo de l'oppening
+    //     metre la chanson de "cha-la-head-cha-la.ogg"
+    //     apres 5 second arrete puis metre le song "HEY_ITS_ME_GOKU.ogg"
+    //     if(on ouvre la porte avec les etapes précédentes)
+    //     goku fonce sur nous en courrant avec le song "goku_drip.ogg"
+    return 0;
+}
