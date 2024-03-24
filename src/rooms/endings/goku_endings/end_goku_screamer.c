6ce57adebc4cc60cff77c0df629c3840d226b227
Matteo Aufiero <matteo.aufiero@epitech.eu>
2024-03-24 21:49:39 +0100

    [FIX] hitboxes in fidge

--- src/rooms/endings/goku_endings/end_goku_screamer.c
+++ src/rooms/endings/goku_endings/end_goku_screamer.c
@@ -12,6 +12,5 @@ void goku_screamer_happen(void)
     while (sfTime_asSeconds(sfClock_getElapsedTime(GAME.clock)) < 1) {
         sfRenderWindow_drawSprite(GAME.window, GAME.items->door, NULL);
         sfRenderWindow_display(GAME.window);
-        sfSprite_setTexture(SPRITE->goku, SPRITE->goku_text, sfTrue);
     }
 }
